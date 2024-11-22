26e40f46cccb05519b524fb3d151d69159c2517d
Mukesh <91588183+tsMukesh51@users.noreply.github.com>
2024-11-22 06:16:09 +0530

    week-5

---
+++ week-5/index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+
+const app = express();
+
+let requestCount = 0;
+function requestCountInc(req, res, next) {
+    requestCount++;
+    console.log(requestCount);
+    next();
+}
+
+app.get("/sum", requestCountInc, function(req, res) {
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+
+    res.json({
+        ans: a + b
+    })
+});
+
+app.get("/multiply", requestCountInc, function(req, res) {
+    const a = req.query.a;
+    const b = req.query.b;
+    res.json({
+        ans: a * b
+    })
+});
+
+app.get("/divide", requestCountInc, function(req, res) {
+    const a = req.query.a;
+    const b = req.query.b;
+    res.json({
+        ans: a / b
+    })
+
+});
+
+app.get("/subtract", requestCountInc, function(req, res) {
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+    res.json({
+        ans: a - b
+    })
+});
+
+app.listen(3000);
