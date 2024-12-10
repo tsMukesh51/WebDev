59a0b70ac916ce3bca7cb8bfbd1480ed0eecfea2
Mukesh <91588183+tsMukesh51@users.noreply.github.com>
2024-12-10 11:09:09 +0530

    Custom Hooks

--- Week-11/src/App.jsx
+++ Week-11/src/App.jsx
@@ -2,12 +2,21 @@ import { useState, useEffect } from 'react'
 import { useFetch } from './hooks/useFetch'
 
 function App() {
-  const {dataa} = useFetch();
+  const [todoId, setTodoId] = useState(1);
+  const { dataa, loading } = useFetch("https://jsonplaceholder.typicode.com/users/" + todoId);
 
   console.log(dataa, ' printing');
   return (
     <div>
-      {dataa ? dataa.title : "Nothing"}
+      <div>
+        <button onClick={() => setTodoId(1)}>1</button>
+        <button onClick={() => setTodoId(2)}>2</button>
+        <button onClick={() => setTodoId(3)}>3</button>
+        <button onClick={() => setTodoId(4)}>4</button>
+      </div>
+      <p>
+        {loading ? "Loading..." : JSON.stringify(dataa)}
+      </p>
     </div>
   )
 }
--- Week-11/src/hooks/useFetch.js
+++ Week-11/src/hooks/useFetch.js
@@ -1,19 +1,56 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
-export function useFetch() {
+export function useFetch(url) {
     const [dataa, setDataa] = useState({});
-    // const [loading, setLoading] = useState(true);
-    
-    async function getData() {
-        const fetchData = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-        const jsonData = await fetchData.json();
-        setDataa(jsonData);
+    const [loading, setLoading] = useState(true);
+    const debounceTimeout = useRef(null);
+    const updateInterval = useRef(null);
+
+    async function getData(url) {
+        console.log('getting...');
+        setLoading(true);
+        try {
+            const fetchData = await fetch(url);
+            const jsonData = await fetchData.json();
+            setDataa(jsonData);
+        } catch (err) {
+            console.log(err.name);
+        } finally {
+            setLoading(false);
+        }
     }
-  
+
+    async function updateData(url) {
+        console.log('updateing...');
+        try {
+            const fetchData = await fetch(url);
+            const jsonData = await fetchData.json();
+            setDataa(jsonData);
+        } catch (err) {
+            console.log(err.name);
+        }
+    }
+
+    // useEffect(() => {
+    //     getData(url);
+    // }, [url]);
+
     useEffect(() => {
-        // setLoading(true);
-        getData();
-        // setLoading(false);
-    },[]);
-    return {dataa};
+        if (debounceTimeout.current)
+            clearTimeout(debounceTimeout.current)
+        debounceTimeout.current = setTimeout(async function () {
+            getData(url);
+            if (updateInterval.current)
+                clearInterval(updateInterval.current);
+            updateInterval.current = setInterval(() => updateData(url), 15000);
+        }, 2000);
+
+        return () => {
+            console.log('unmounting...');
+            clearInterval(updateInterval.current);
+            clearTimeout(debounceTimeout.current);
+        }
+    }, [url]);
+
+    return { dataa, loading };
 } 
