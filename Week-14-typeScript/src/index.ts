e64057bf52e73e00bd04492faaa0e2a11478798d
Mukesh <91588183+tsMukesh51@users.noreply.github.com>
2024-12-17 15:24:31 +0000

    TS prac

--- Week-14-typeScript/src/index.ts
+++ Week-14-typeScript/src/index.ts
@@ -9,4 +9,65 @@ const sum = (a: sumInp, b: sumInp): sumInp => {
 let x = 1
 let b = '4'
 
-console.log(sum(x, b));
+// console.log(sum(x, b));
+
+interface User {
+    name: string,
+    age: number,
+    address?: {
+        city: string,
+        country: string,
+        pincode: number
+    }
+}
+
+const users: User[] = [
+    {
+        name: 'Mukesh',
+        age: 19,
+        address: {
+            city: 'Bengaluru',
+            country: 'Bharath',
+            pincode: 560000
+        }
+    },
+    {
+        name: 'Mukesh2',
+        age: 18,
+        address: {
+            city: 'Bengaluru',
+            country: 'Bharath',
+            pincode: 560000
+        }
+    },
+    {
+        name: 'Mukesh3',
+        age: 9,
+        address: {
+            city: 'Bengaluru',
+            country: 'Bharath',
+            pincode: 560000
+        }
+    },
+    {
+        name: 'Mukesh4',
+        age: 194,
+        address: {
+            city: 'Bengaluru',
+            country: 'Bharath',
+            pincode: 560000
+        }
+    }
+]
+
+const isLeagal = (users: User[]): User[] => {
+    return users.filter((user: User): boolean => {
+        return user.age >= 18 && user.age <= 90;
+    });
+}
+
+const printUsers = (users: User[]): void => {
+    users.forEach((user: User): void => console.log(user.name));
+}
+
+printUsers(isLeagal(users));
