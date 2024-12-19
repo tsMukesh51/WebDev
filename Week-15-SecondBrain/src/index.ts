d7eaeef06c6c76d0a4e2f633c37d13554ca53e01
Mukesh <91588183+tsMukesh51@users.noreply.github.com>
2024-12-19 17:33:38 +0000

    User Authentication

--- Week-15-SecondBrain/src/index.ts
+++ Week-15-SecondBrain/src/index.ts
@@ -1,14 +1,16 @@
 import express from "express";
 import { json } from "express";
-import mongoose from "mongoose";
+import mongoose, { Error } from "mongoose";
 import jwt from "jsonwebtoken";
 import { string, z } from "zod";
+import dotenv from "dotenv"
 
 import { userSchema } from "./schema";
 import { userModel } from "./db";
 
 const app = express();
 app.use(json());
+dotenv.config();
 
 app.post("api/v1/signup", async (req, res) => {
     const { success, data, error } = userSchema.safeParse(req.body);
@@ -25,14 +27,59 @@ app.post("api/v1/signup", async (req, res) => {
             msg: 'Account created'
         });
     } catch (err) {
-        res.json({
+        if (err.code == '11000') {
+            res.status(403).json({
+                msg: 'Email already in use'
+            });
+        }
+        res.status(403).json({
             msg: 'Account already exists'
-        })
+        });
     }
 });
 
-app.post("api/v1/signin", (req, res) => {
-
+app.post("api/v1/signin", async (req, res) => {
+    const { success, data, error } = userSchema.pick({
+        username: true,
+        password: true,
+    }).safeParse(req.body);
+    if (!success) {
+        res.status(411).json({
+            msg: 'Invalid Format',
+            err: error.errors
+        })
+        return;
+    }
+    try {
+        const currentUser = await userModel.findOne(req.body.username);
+        if (currentUser) {
+            if (currentUser.password === req.body.password) {
+                if (process.env.JWT_SECRET) {
+                    const jwttoken = jwt.sign({ username: currentUser.username }, process.env.JWT_SECRET);
+                    res.json({
+                        msg: 'Login Successful',
+                        token: `Bearer ${jwttoken}`
+                    });
+                } else {
+                    res.status(500).json({
+                        msg: 'Internal Server Error'
+                    })
+                }
+            } else {
+                res.status(403).json({
+                    msg: 'Wrong Credentials'
+                });
+            }
+        } else {
+            res.status(403).json({
+                msg: 'Account does not exists'
+            });
+        }
+    } catch (err) {
+        res.status(500).json({
+            msg: 'Internal Server Error'
+        });
+    }
 });
 
 app.get("api/v1/content", (req, res) => {
--- Week-15-SecondBrain/src/middleware/users.ts
+++ Week-15-SecondBrain/src/middleware/users.ts
@@ -1,7 +1,18 @@
-import { request, response } from "express";
-import { z } from "zod";
+import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
 
-export const signUp = (request: Request, response, next): void => {
-
+export const userAuth = (req: Request, res: Response, next: () => void): void => {
+    const token = req.headers.authorization?.split(' ')[1];
+    try {
+        if (token && process.env.JWT_SECRET) {
+            const userId = jwt.verify(token, process.env.JWT_SECRET);
+            req.body.userId = userId;
+            next();
+        }
+    } catch (err) {
+        res.status(403).json({
+            msg: 'Please login again'
+        })
+    }
 }
 
