fa6475e8568338efd25381663f86ab719145a64f
Turbobot <turbobot@vercel.com>
2025-03-07 07:30:28 +0530

    feat(create-turbo): apply official-starter transform

--- proj-week-23-drawinsync/apps/frontend/src/app/page.tsx
+++ proj-week-23-drawinsync/apps/frontend/src/app/page.tsx
@@ -4,9 +4,14 @@ import { Button } from "@repo/ui/button";
 
 export default function Home() {
   return (
-    <div>
-      <div>Home</div>
-      <Button className="nothing" >Submit</Button>
-    </div>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <div>
+        <div>Home</div>
+        <Button className="bg-red-100" >Submit</Button>
+      </div>
+      <h1 className="text-4xl font-bold text-blue-500">
+        Hello Tailwind!
+      </h1>
+    </main>
   );
 }
